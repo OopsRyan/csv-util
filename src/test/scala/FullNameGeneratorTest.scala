d65b0a2b945fd4e46938ddb8c9fa89cce54807f6
Ryan <qirun_chen@163.com>
2018-10-18 16:53:28 +0100

    add test case for empty iterator in FullNameGenerator

--- src/test/scala/FullNameGeneratorTest.scala
+++ src/test/scala/FullNameGeneratorTest.scala
@@ -3,12 +3,12 @@ import qirun.utils._
 
 class FullNameGeneratorTest extends FlatSpec with Matchers {
 
-  val firstnames = CSVParser.read("src/test/resources/firstnames_samples.csv")
-  val lastnames = CSVParser.read("src/test/resources/lastnames_samples.csv")
-
-  val generator = FullNameGenerator(firstnames, lastnames)
-
   "FullnameGenerator" should "return the next element in the iterator" in {
+
+    val firstnames = CSVParser.read("src/test/resources/firstnames_samples.csv")
+    val lastnames = CSVParser.read("src/test/resources/lastnames_samples.csv")
+
+    val generator = FullNameGenerator(firstnames, lastnames)
 
     assert(generator.next() == ("Aaron", "Aaberg"))
     assert(generator.next() == ("Aaron", "Aaby"))
@@ -16,7 +16,11 @@ class FullNameGeneratorTest extends FlatSpec with Matchers {
 
   "FullnameGenerator" should "return the empty iterator when it reaches the last one" in {
 
-    assert(generator.next() == ("Aaron", "Aaberg"))
-    assert(generator.next() == ("Aaron", "Aaby"))
+    val empty1 = CSVParser.read("src/test/resources/empty.csv")
+    val empty2 = CSVParser.read("src/test/resources/empty.csv")
+
+    val generator = FullNameGenerator(empty1, empty2)
+
+    assert(generator.next() == generator.iterator)
   }
 }
